a5758e5ad9a099dc1d7279a70c2e17d6c4c4935c
SeyedMohammad Nasiry <seyedmohammad.nasiry@gmail.com>
2024-12-26 22:14:51 +0330

    refactor: make articles and projects part dynamic using models.

---
+++ DevCard_MVC/Models/IndexModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace DevCard_MVC.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Client { get; set; }
+    }
+
+    public class Article
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public class IndexModel
+    {
+        public List<Project> Projects { get; set; }
+
+        public List<Article> Articles { get; set; }
+    }
+}
