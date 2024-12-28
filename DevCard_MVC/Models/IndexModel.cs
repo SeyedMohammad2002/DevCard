4809154859c182bdf19206b09561af47f5ca91a6
SeyedMohammad Nasiry <seyedmohammad.nasiry@gmail.com>
2024-12-28 10:21:36 +0330

    feat: using patrial views to make projects and articles part ddynamic

--- DevCard_MVC/Models/IndexModel.cs
+++ DevCard_MVC/Models/IndexModel.cs
@@ -2,26 +2,6 @@
 
 namespace DevCard_MVC.Models
 {
-    public class Project
-    {
-        public long Id { get; set; }
-
-        public string Name { get; set; }
-
-        public string Description { get; set; }
-
-        public string Client { get; set; }
-    }
-
-    public class Article
-    {
-        public long Id { get; set; }
-
-        public string Title { get; set; }
-
-        public string Description { get; set; }
-    }
-
     public class IndexModel
     {
         public List<Project> Projects { get; set; }
