df4194c06c97fc0dcc65723d068e379b6954dbe5
SeyedMohammad Nasiry <seyedmohammad.nasiry@gmail.com>
2024-12-29 12:05:39 +0330

    feat: Connect contact form to home controller, Contact action with model binding.

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,19 +1,34 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Security.AccessControl;
 using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
         {
             return View("Index");
         }
 
+        [HttpGet]
         public IActionResult Contact()
         {
             return View("Contact");
+        }
+
+        //[HttpPost]
+        //public JsonResult Contact(IFormCollection form)
+        //{
+        //    return Json(Ok()); // Ok() is a helper method that returns a 200 OK status code
+        //}
+        [HttpPost]
+        public JsonResult Contact(Contact form)
+        {
+            return Json(Ok());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
