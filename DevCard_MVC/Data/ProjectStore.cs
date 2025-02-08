17c54020ca524c37454e1ea999ec872c356f28ad
SeyedMohammad Nasiry <seyedmohammad.nasiry@gmail.com>
2025-02-08 14:16:31 +0330

    feat: Add projects view components.

---
+++ DevCard_MVC/Data/ProjectStore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using DevCard_MVC.Models;
+
+namespace DevCard_MVC.Data
+{
+    public class ProjectStore
+    {
+        public static List<Project> GetProjects()
+        {
+            return new List<Project>
+            {
+                new(1, "ZoodFood", "درخواست آنلاین غذا برای سراسر کشور", "ZoodFood", "project-1.jpg"),
+                new(2, "سامانه مدارس", "سیستم مدیریت یکپارچه مدارس", "وزارت آموزش و پرورش", "project-2.jpg"),
+                new(3, "هی تاکسی", "درخواست آنلاین سفر های درون شهری", "Taxi.Co", "project-3.jpg"),
+                new(4, "InteliPredict", "تخمین قیمت کالا ها در بازه های زمانی مختلف", "Finance", "project4.jpg")
+            };
+        }
+    }
+}
--- DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using DevCard_MVC.Data;
 using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 
 namespace DevCard_MVC.ViewComponents
 {
@@ -8,13 +10,7 @@ namespace DevCard_MVC.ViewComponents
     {
         public IViewComponentResult Invoke()
         {
-            var projects = new List<Project>
-            {
-                new(1, "ZoodFood", "درخواست آنلاین غذا برای سراسر کشور", "ZoodFood", "project-1.jpg"),
-                new(2, "سامانه مدارس", "سیستم مدیریت یکپارچه مدارس", "وزارت آموزش و پرورش", "project-2.jpg"),
-                new(3, "هی تاکسی", "درخواست آنلاین سفر های درون شهری", "Taxi.Co", "project-3.jpg"),
-                new(4, "InteliPredict", "تخمین قیمت کالا ها در بازه های زمانی مختلف", "Finance", "project4.jpg")
-            };
+            var projects = ProjectStore.GetProjects();
             return View("_Projects", projects);
             //Return view components
             //there is no need to Mother model in the controller.
